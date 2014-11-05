c90644ad58241935a54f55009ed91255fa02a43b
Ricky Pai <rickyp999@gmail.com>
2014-11-05 11:38:14 -0800

    add stageit to cgit

--- Packages/User/cgit.py
+++ Packages/User/cgit.py
@@ -3,7 +3,7 @@ import sublime, sublime_plugin
 
 # creates a cgit link to the line of the cursor location, on the working git branch
 
-repos = ("birdcage", "science", "peacock", "tfe")
+repos = ("birdcage", "science", "peacock", "tfe", "stageit")
 
 class CgitCommand(sublime_plugin.TextCommand):
 
