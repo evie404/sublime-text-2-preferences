daf9d983585828b820ddd994bd2651478dd732cd
Ricky Pai <ricky.pai@airbnb.com>
2015-08-12 16:41:24 -0700

    add more apps to github_link script

--- Packages/User/github_link.py
+++ Packages/User/github_link.py
@@ -3,7 +3,7 @@ import sublime, sublime_plugin
 
 # creates a github link to the line of the cursor location, on the working git branch
 
-REPOS = ["airbnb", "restore"]
+REPOS = ["airbnb", "restore", "chef", "milestones", "deployboard"]
 BASE_URL = "https://git.musta.ch/airbnb/"
 
 class GithubLinkCommand(sublime_plugin.TextCommand):
