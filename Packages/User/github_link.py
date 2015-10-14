60d26e286f6c337976c67bcd3a0b15d389849f2f
Ricky Pai <ricky.pai@airbnb.com>
2015-10-14 14:17:28 -0700

    updates

--- Packages/User/github_link.py
+++ Packages/User/github_link.py
@@ -3,7 +3,7 @@ import sublime, sublime_plugin
 
 # creates a github link to the line of the cursor location, on the working git branch
 
-REPOS = ["airbnb", "restore", "chef", "milestones", "deployboard", "tungsten-support"]
+REPOS = ["airbnb", "restore", "chef", "milestones", "deployboard", "tungsten-support", "rails", "tungsten"]
 BASE_URL = "https://git.musta.ch/airbnb/"
 
 class GithubLinkCommand(sublime_plugin.TextCommand):
@@ -15,14 +15,23 @@ class GithubLinkCommand(sublime_plugin.TextCommand):
     branch_cmd = "cd %s; git rev-parse --abbrev-ref HEAD" % dir_path
 
     repo_directory = os.popen(repo_directory_cmd).read()[:-1]
+
+    print(repo_directory)
+
+
     branch_name = branch_override or os.popen(branch_cmd).read()[:-1]
     repo_name = self._get_repo_name(repo_directory)
+
+    print(repo_name)
+    print(file_name)
+    print(file_name.startswith(repo_directory))
+
 
     if repo_name is None:
       return
 
-    if not file_name.startswith(repo_directory):
-      return
+    # if not file_name.startswith(repo_directory):
+    #   return
 
     github_link = BASE_URL + "%(repo_name)s/blob/%(branch_name)s%(file_name_path)s%(line_number)s" % {
       "repo_name": repo_name,
